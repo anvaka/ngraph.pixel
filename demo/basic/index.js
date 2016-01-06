0df249506705fc77806a598a79655357d03ef617
anvaka <anvaka@gmail.com>
2016-01-05 23:41:46 -0800

    formatting

--- demo/basic/index.js
+++ demo/basic/index.js
@@ -2,10 +2,10 @@ var query = require('query-string').parse(window.location.search.substring(1));
 var graph = getGraphFromQueryString(query);
 var renderGraph = require('../../');
 
-window.r =renderGraph(graph);
+renderGraph(graph);
 
 function getGraphFromQueryString(query) {
-   var graphGenerators = require('ngraph.generators');
+  var graphGenerators = require('ngraph.generators');
   var createGraph = graphGenerators[query.graph] || graphGenerators.grid;
   return createGraph(getNumber(query.n), getNumber(query.m), getNumber(query.k));
 }
