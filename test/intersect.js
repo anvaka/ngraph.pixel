5892eb0a54ddbcc209a5ddec0440ce0436355236
anvaka <anvaka@gmail.com>
2016-01-22 18:17:25 -0800

    Added test for random points

--- test/intersect.js
+++ test/intersect.js
@@ -16,6 +16,30 @@ test('it finds intersection', function(t) {
   t.end();
 });
 
+test('it finds intersection between random points', function(t) {
+  for (var i = 0; i < 100; ++i) {
+    var sphereCenter = getRandomPoint();
+    var sphereRadius = Math.random() * 10;
+    var point = getRandomPoint();
+
+    var intersection = intersect(point, sphereCenter, sphereRadius);
+    var dx = (intersection.x - sphereCenter.x);
+    var dy = (intersection.y - sphereCenter.y);
+    var dz = (intersection.z - sphereCenter.z);
+    var distanceToPoint = Math.sqrt(dx * dx + dy * dy + dz * dz);
+    t.ok(close(distanceToPoint, sphereRadius), 'Point OK: ' + JSON.stringify({
+      sphereCenter: sphereCenter,
+      sphereRadius: sphereRadius,
+      point: point
+    }))
+  }
+  t.end();
+
+  function getRandomPoint() {
+    return { x: Math.random() * 100, y: Math.random() * 100, z: Math.random() * 100 };
+  }
+});
+
 function close(x, y) {
   return Math.abs(x - y) < 0.1e-10;
 }
