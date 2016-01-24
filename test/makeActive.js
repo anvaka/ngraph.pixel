17805a29d32632f4da1c3214888b8e61df38bc45
anvaka <anvaka@gmail.com>
2016-01-24 12:57:53 -0800

    Added nested connector test

--- test/makeActive.js
+++ test/makeActive.js
@@ -13,6 +13,7 @@ test('it can listen to actived objects', function(t) {
     t.end();
   }
 });
+
 test('it can unsubscribe from activated objects', function(t) {
   var obj = makeActive({ name: 'John' });
   var calledCount = 0;
@@ -24,9 +25,20 @@ test('it can unsubscribe from activated objects', function(t) {
   t.equals(calledCount, 1, 'Called exactly once');
   t.end();
 
-
   function onNameChanged() {
     calledCount += 1;
     t.equals(calledCount, 1, 'Called once!');
+  }
+});
+
+test('it can listen to nested objects', function(t) {
+  var obj = makeActive({ user: {name: 'John' }});
+  obj.connect('user.name', onNameChanged);
+  obj.user.name = 'Bob';
+
+  function onNameChanged(model) {
+    t.ok(model === obj, 'model is the same as source object');
+    t.equals(model.user.name, 'Bob', 'Name is changed');
+    t.end();
   }
 });
